886e59f44bcd27f342ce4eb659adfe81f0fc8192
Per Olofsson <Per.Olofsson@assaabloy.com>
2018-01-05 22:15:00 +0100

    Added GetSpace Properties

--- client/space.go
+++ client/space.go
@@ -20,12 +20,22 @@ type SpaceType struct {
 	Key        string `json:"key,omitempty" structs:"key,omitempty"`
 	Name       string `json:"name,omitempty" structs:"name,omitempty"`
 	Type       string `json:"type,omitempty" structs:"type,omitempty"`
-	Links      string `json:"links,omitempty" structs:"links,omitempty"`
+	Links      string `json:"_links,omitempty" structs:"_links,omitempty"`
 	Start      string `json:"start,omitempty" structs:"start,omitempty"`
 	Expandable string `json:"_expandable,omitempty" structs:"_expandable,omitempty"`
 }
 
-//SearchPages searches for pages in the space that meet the specified criteria
+type ConfluenceSpacePropertyResult struct {
+	Results []string `json:"results,omitempty" structs:"results,omitempty"`
+	Start   int         `json:"start,omitempty" structs:"start,omitempty"`
+	Limit   int         `json:"limit,omitempty" structs:"limit,omitempty"`
+	Size    int         `json:"size,omitempty"  structs:"size,omitempty`
+	Links      string `json:"_links,omitempty" structs:"_links,omitempty"`
+	Base      string `json:"base,omitempty" structs:"base,omitempty"`
+	Context      string `json:"context,omitempty" structs:"context,omitempty"`
+}
+
+//GetSpaces searches for pages in the space that meet the specified criteria
 func (c *ConfluenceClient) GetSpaces(options *SpaceOptions) (results *ConfluenceSpaceResult) {
 	var req string
 	if options == nil {
@@ -34,6 +44,16 @@ func (c *ConfluenceClient) GetSpaces(options *SpaceOptions) (results *Confluence
 		req = fmt.Sprintf("/rest/api/space?start=%d&limit=%d", options.Start, options.Limit)
 	}
 	results = new (ConfluenceSpaceResult)
+	c.doRequest("GET", req, nil, results)
+	return results
+}
+
+
+//http://example.com/rest/experimental/space/TST/property?expand=space,version
+func (c *ConfluenceClient) GetSpaceProperties(spacekey string ) (results *ConfluenceSpacePropertyResult) {
+	var req string
+	req = fmt.Sprintf("/rest/api/space/%s/property?expand=space,version", spacekey)
+	results = new (ConfluenceSpacePropertyResult)
 	c.doRequest("GET", req, nil, results)
 	return results
 }
